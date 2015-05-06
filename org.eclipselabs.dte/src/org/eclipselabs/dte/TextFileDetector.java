db85568c2b5d0b3d11a6d51c47b63ff80d87fd2f
Kaloyan Raev <kaloyan@raev.name>
2015-05-06 16:17:49 +0300

    Improve Javadoc in TextFileDetector

--- org.eclipselabs.dte/src/org/eclipselabs/dte/TextFileDetector.java
+++ org.eclipselabs.dte/src/org/eclipselabs/dte/TextFileDetector.java
@@ -16,16 +16,25 @@ import java.io.InputStream;
 import com.ibm.icu.text.CharsetDetector;
 import com.ibm.icu.text.CharsetMatch;
 
+/**
+ * Utility class for detecting if file is text or binary.
+ */
 public class TextFileDetector {
 
     /**
      * Checks if a file is text or binary.
+     * 
+     * <p>
+     * The provided input stream will not be closed and the position in the
+     * stream will be unchanged after leaving this method.
+     * </p>
      * 
      * @param is
      *            input stream of the file
      * @return <code>true</code> if the file is more likely to be text file than
      *         binary file, <code>false</code> otherwise.
      * @throws IOException
+     *             in case of error while reading from the stream
      */
     public static boolean isTextFile(InputStream is) throws IOException {
         CharsetDetector detector = new CharsetDetector();
